5a2a950a5bb8ec965db78c6e612916f22e7d8919
Maciej Golaszewski <maciej.golaszewski@wroclaw.merito.pl>
2024-01-28 15:34:43 +0100

    fmt fix

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,18 +1,18 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.89.0"
     }
   }
-    backend "azurerm" {
-      resource_group_name  = "maciektf-resources"
-      storage_account_name = "maciektfstoracc"
-      container_name       = "maciektfcontent"
-      key                  = "terraform.tfstate"
+  backend "azurerm" {
+    resource_group_name  = "maciektf-resources"
+    storage_account_name = "maciektfstoracc"
+    container_name       = "maciektfcontent"
+    key                  = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
