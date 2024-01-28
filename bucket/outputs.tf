cec20a86f302349acdc555aa67351970c2c1dbc6
Maciej Golaszewski <maciej.golaszewski@wroclaw.merito.pl>
2024-01-28 14:29:06 +0100

    aks-code addresses #3

--- bucket/outputs.tf
+++ bucket/outputs.tf
@@ -1,3 +1,13 @@
 output "storage_id" {
   value = azurerm_storage_container.example.id
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}
+output "container_name" {
+  value = azurerm_storage_container.example.name
+}
---
+++ infra/maciek.tfvars
@@ -0,0 +1,2 @@
+name="maciek"
+location="West Europe"
